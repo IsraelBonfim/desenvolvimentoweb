112fe3a0b4e75b42d70e2fae737697c69ad2d8dc
Israel Bonfim <israelsilva615@gmail.com>
2018-07-17 17:52:09 -0300

    ola mundo na linguagem PHP

---
+++ ola-mundo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+   <meta charset="UTF-8"/>
+   <title>Primeiro Exenlo PHP</title>
+   <style>
+       h2 {
+           color: #80a2ff;
+       }
+    </style>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <?php
+    echo "<h2>Olá Mundo!</h2>";  
+    ?>
+</body>
+</html>
