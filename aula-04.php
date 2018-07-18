9d4de81ed23adf9b0e64a025ba80134ad2568b1e
Israel Bonfim <israelsilva615@gmail.com>
2018-07-18 10:49:55 -0300

    Aula 04 do curso PHP-Variáveis

---
+++ aula-04.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+   <meta charset="UTF-8"/>
+   <link rel="stylesheet" href="_css/estilo.css">
+   <title></title>
+</head>
+<body>
+<div>
+    <?php
+      $idade = 36 ;
+      $nome = "Gustavo ";
+      echo $nome . "tem ". $idade." anos";
+      ?>
+</div>
+</body>
+</html>
