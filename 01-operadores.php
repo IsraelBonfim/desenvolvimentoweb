ad8c8aab0a825afec0c8c2d7761399bddeb7654e
Israel Bonfim <israelsilva615@gmail.com>
2018-07-19 13:05:50 -0300

    exenplo de operadores de atribuiçao em PHP Aula06

---
+++ 01-operadores.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Israel Bonfim-PHP</title>
+</head>
+<body>
+<div>
+    <?php
+       $preco = $_GET["p"];
+       echo "O preco do produto e R$ " . number_format($preco, 2);
+       $preco -= $preco*10/100;
+       echo "<br/>E o novo preco com 10% de desconto sera R$ " . number_format($preco, 2);
+    ?>
+</div>
+</body>
+</html>
+ 
