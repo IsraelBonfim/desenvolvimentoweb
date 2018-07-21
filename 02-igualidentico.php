52b0fc6e0199e410d65c30aa076210f3d73f246b
Israel Bonfim <israelsilva615@gmail.com>
2018-07-21 10:20:01 -0300

    testar se uma variavel é igual ou identica a outra

---
+++ 02-igualidentico.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="_css/estilo.css"/>
+  <meta charset="UTF-8"/>
+  <title>Israel Bonfim-PHP</title>
+</head>
+<body>
+<div>
+    <?php
+       $a = 3;
+       $b = "3";
+       $r = ($a === $b)?"SIM":"NAO";
+       echo "As variaveis A e B sao identicas? $r";
+    ?>
+</div>
+</body>
+</html>
+ 
